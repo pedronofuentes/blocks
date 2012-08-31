383b65bb0f0caf58c54ff3a4d72168d09114bd83
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2012-08-31 14:00:19 +0200

    Implemented BlockLibrary methods

--- block_library.py
+++ block_library.py
@@ -9,16 +9,23 @@ class BlockLibrary():
     """Manages block_library"""
 
     def add_block(self, block):
-        pass
+        block_library['%s.%s' % (block.__module__, block.__name__)] = block
 
     def get_block(self, block):
-        pass
+        try:
+            return block_library[block]
+        except KeyError:
+            raise NotInLibraryException('%s is not in the library' % block)
 
     def get_blocks(self):
-        pass
+        return block_library
 
     def remove_block(self, block):
-        pass
+        try:
+            del block_library[block]
+        except KeyError:
+            raise NotInLibraryException('%s is not in the library' % block)
 
     def remove_all_blocks(self):
-        pass
+        for key in block_library.keys():
+            del block_library[key]
