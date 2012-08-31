7d6afbbacd26edbd7579d9cbaf4a93125af476cf
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2012-08-31 13:20:42 +0200

    Added initial structure of BlockLibrary

---
+++ block_library.py
@@ -0,0 +1,24 @@
+block_library = {}
+
+
+class NotInLibraryException(Exception):
+    pass
+
+
+class BlockLibrary():
+    """Manages block_library"""
+
+    def add_block(self):
+        pass
+
+    def get_block(self, block):
+        pass
+
+    def get_blocks(self):
+        pass
+
+    def remove_block(self, block):
+        pass
+
+    def remove_all_blocks(self):
+        pass
