31fb1715e92bc776a096c4ba2d045933866e8426
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2012-08-31 17:07:59 +0200

    Added autodiscover method

--- block_library.py
+++ block_library.py
@@ -1,3 +1,9 @@
+import re
+import os
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.importlib import import_module
+
 block_library = {}
 
 
@@ -29,3 +35,16 @@ class BlockLibrary():
     def remove_all_blocks(self):
         for key in block_library.keys():
             del block_library[key]
+
+    def autodiscover(self, apps=settings.INSTALLED_APPS):
+        for app in apps:
+            if not re.match('[django.*|blocks]', app):
+                try:
+                    mod = import_module(app)
+                except ImportError as e:
+                    raise ImproperlyConfigured('ImportError %s: %s' % (app, e.args[0]))
+                if os.path.isfile(os.path.join(os.path.dirname(mod.__file__), 'blocks.py')):
+                    block_module = import_module('%s.%s' % (app, 'blocks'))
+                    for x in dir(block_module):
+                        if callable(getattr(block_module, x)) and re.match(r'.*_block', x):
+                            self.add_block(getattr(block_module, x))
