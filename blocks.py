9bdb56629d8f69d90287ddb8889e7bec8fdf85c5
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2012-09-01 13:14:48 +0200

    Tests modified to get block functions from block.py

---
+++ blocks.py
@@ -0,0 +1,13 @@
+# File contains block functions for testing
+
+
+def foo_block():
+    pass
+
+
+def another_foo_block():
+    pass
+
+
+def render_content_foo_block():
+    return 'Hello World!'
--- tests.py
+++ tests.py
@@ -1,13 +1,6 @@
 from django.test import TestCase
-from blocks import BlockLibrary, NotInLibraryException
-
-
-def foo_block():
-    pass
-
-
-def another_foo_block():
-    pass
+from block_library import BlockLibrary, NotInLibraryException
+from blocks import foo_block, another_foo_block
 
 
 class BlockLibraryTest(TestCase):
@@ -23,7 +16,7 @@ class BlockLibraryTest(TestCase):
         """
         self.block_library.add_block(foo_block)
         self.assertEqual(len(self.block_library.get_blocks()), 1, 'Number of blocks in library shoud be 1')
-        self.assertIn('blocks.tests.foo_block', self.block_library.get_blocks(), 'blocks.tests.foo_block key should be in the library dictionary')
+        self.assertIn('blocks.blocks.foo_block', self.block_library.get_blocks(), 'blocks.blocks.foo_block key should be in the library dictionary')
 
     def test_get_block(self):
         """
@@ -31,9 +24,9 @@ class BlockLibraryTest(TestCase):
         """
         self.block_library.add_block(foo_block)
         self.assertEqual(len(self.block_library.get_blocks()), 1, 'Number of blocks in library shoud be 1')
-        self.assertIn('blocks.tests.foo_block', self.block_library.get_blocks(), 'blocks.tests.foo_block key should be in the library dictionary')
-        self.assertIn('__call__', dir(self.block_library.get_block('blocks.tests.foo_block')), 'BlockLibrary.get_block should return a function')
-        self.assertIs(self.block_library.get_block('blocks.tests.foo_block'), foo_block, 'BlockLibrary.get_block should return foo_block')
+        self.assertIn('blocks.blocks.foo_block', self.block_library.get_blocks(), 'blocks.blocks.foo_block key should be in the library dictionary')
+        self.assertIn('__call__', dir(self.block_library.get_block('blocks.blocks.foo_block')), 'BlockLibrary.get_block should return a function')
+        self.assertIs(self.block_library.get_block('blocks.blocks.foo_block'), foo_block, 'BlockLibrary.get_block should return foo_block')
 
     def test_get_blocks(self):
         """
@@ -43,8 +36,8 @@ class BlockLibraryTest(TestCase):
         self.block_library.add_block(another_foo_block)
         the_library = self.block_library.get_blocks()
         self.assertEqual(len(the_library), 2, 'the_library length shoud be 2')
-        self.assertIn('blocks.tests.foo_block', the_library, 'blocks.tests.foo should be in the_library')
-        self.assertIn('blocks.tests.another_foo_block', the_library, 'blocks.tests.another_foo_block should be in the_library')
+        self.assertIn('blocks.blocks.foo_block', the_library, 'blocks.blocks.foo should be in the_library')
+        self.assertIn('blocks.blocks.another_foo_block', the_library, 'blocks.blocks.another_foo_block should be in the_library')
 
     def test_get_block_not_in_library(self):
         """
@@ -59,8 +52,8 @@ class BlockLibraryTest(TestCase):
         self.block_library.add_block(foo_block)
         self.block_library.add_block(another_foo_block)
         self.assertEqual(len(self.block_library.get_blocks()), 2, 'Number of blocks in the library shoud be 2')
-        self.assertIn('blocks.tests.foo_block', self.block_library.get_blocks(), 'blocks.tests.foo_block key should be in the library dictionary')
-        self.assertIn('blocks.tests.another_foo_block', self.block_library.get_blocks(), 'blocks.tests.another_foo_block key should be in the library dictionary')
+        self.assertIn('blocks.blocks.foo_block', self.block_library.get_blocks(), 'blocks.blocks.foo_block key should be in the library dictionary')
+        self.assertIn('blocks.blocks.another_foo_block', self.block_library.get_blocks(), 'blocks.blocks.another_foo_block key should be in the library dictionary')
         self.block_library.remove_all_blocks()
         self.assertEqual(len(self.block_library.get_blocks()), 0, 'Number of blocks in the library should be 0')
 
@@ -71,13 +64,23 @@ class BlockLibraryTest(TestCase):
         self.block_library.add_block(foo_block)
         self.block_library.add_block(another_foo_block)
         self.assertEqual(len(self.block_library.get_blocks()), 2, 'Number of blocks in the library shoud be 2')
-        self.assertIn('blocks.tests.foo_block', self.block_library.get_blocks(), 'blocks.tests.foo_block key should be in the library dictionary')
-        self.assertIn('blocks.tests.another_foo_block', self.block_library.get_blocks(), 'blocks.tests.another_foo_block key should be in the library dictionary')
-        self.block_library.remove_block('blocks.tests.another_foo_block')
-        self.assertNotIn('blocks.tests.another_foo_block', self.block_library.get_blocks(), 'blocks.tests.another_foo_block should not be in the library dictionary')
+        self.assertIn('blocks.blocks.foo_block', self.block_library.get_blocks(), 'blocks.blocks.foo_block key should be in the library dictionary')
+        self.assertIn('blocks.blocks.another_foo_block', self.block_library.get_blocks(), 'blocks.blocks.another_foo_block key should be in the library dictionary')
+        self.block_library.remove_block('blocks.blocks.another_foo_block')
+        self.assertNotIn('blocks.blocks.another_foo_block', self.block_library.get_blocks(), 'blocks.blocks.another_foo_block should not be in the library dictionary')
 
     def test_remove_block_not_in_library(self):
         """
         Tests that if the block wanted to remove is not in the library raises a NotInLibraryException
         """
         self.assertRaisesRegexp(NotInLibraryException, 'foo.blocks.a_block is not in the library', self.block_library.remove_block, 'foo.blocks.a_block')
+
+    def test_autodiscover_blocks(self):
+        """
+        Tests that BlockLibrary discovers all functions declared at blocks.py
+        """
+        self.block_library.autodiscover()
+        self.assertEqual(len(self.block_library.get_blocks()), 3, 'Number of blocks in the library shoud be 3')
+        self.assertIn('blocks.blocks.foo_block', self.block_library.get_blocks(), 'blocks.blocks.foo_block key should be in the library dictionary')
+        self.assertIn('blocks.blocks.another_foo_block', self.block_library.get_blocks(), 'blocks.blocks.another_foo_block key should be in the library dictionary')
+        self.assertIn('blocks.blocks.render_content_foo_block', self.block_library.get_blocks(), 'blocks.blocks.render_content_foo_block key should be in the library dictionary')
