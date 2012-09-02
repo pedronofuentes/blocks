c766debe498fab0c838c36668349f27229b416d1
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2012-09-02 18:34:34 +0200

    Added test for get_block tag accepting an argument

--- tests.py
+++ tests.py
@@ -123,3 +123,16 @@ class BlockTagsTest(TestCase):
         )
         c = Context({})
         self.assertEqual(t.render(c), '')
+
+    def test_get_block_tag_accepts_argument(self):
+        t1 = Template(
+            '{% load block_tags %}'
+            '{% get_block "blocks.blocks.render_content_with_arg_foo_block" "John" %}'
+        )
+        c = Context({})
+        self.assertEqual(t1.render(c), 'Hello John!')
+        t2 = Template(
+            '{% load block_tags %}'
+            '{% get_block "blocks.blocks.render_content_with_arg_foo_block" "Anne" %}'
+        )
+        self.assertEqual(t2.render(c), 'Hello Anne!')
