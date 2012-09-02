70529dc04fa23ff528827d266bd6c415583293fb
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2012-09-02 19:05:26 +0200

    Refactored get_block tag to accept an argument

--- templatetags/block_tags.py
+++ templatetags/block_tags.py
@@ -4,14 +4,35 @@ from blocks.block_library import BlockLibrary, NotInLibraryException
 register = template.Library()
 
 
-@register.simple_tag
-def get_block(block_name):
-    block_library = BlockLibrary()
-    block_library.autodiscover()
+@register.tag
+def get_block(parser, token):
     try:
-        return block_library.get_block(block_name)()
-    except NotInLibraryException:
-        return ''
+        elements = token.split_contents()
+        tag_name = elements[0]
+        arguments = elements[1:]
+    except ValueError:
+        raise template.TemplateSyntaxError("%r tag requires a single argument" % token.contents.split()[0])
+    if len(arguments) > 2:
+        raise template.TemplateSyntaxError("%r tag takes as much 2 arguments" % tag_name)
+    return BlockNode(arguments)
+
+
+class BlockNode(template.Node):
+    def __init__(self, arguments):
+        self.block_library = BlockLibrary()
+        self.block_library.autodiscover()
+        self.block_name = arguments[0][1:-1]
+        self.argument = arguments[1][1:-1] if len(arguments) > 1 else None
+
+    def render(self, context):
+        try:
+            block = self.block_library.get_block(self.block_name)
+        except NotInLibraryException:
+            return ''
+        if not self.argument:
+            return block()
+        else:
+            return block(self.argument)
 
 
 @register.simple_tag
