28f62201dc8fd05a13ed1272ca4fcf83da4d9a47
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2012-09-02 18:24:49 +0200

    Added get_block_with_arg custom tag

--- templatetags/block_tags.py
+++ templatetags/block_tags.py
@@ -12,3 +12,13 @@ def get_block(block_name):
         return block_library.get_block(block_name)()
     except NotInLibraryException:
         return ''
+
+
+@register.simple_tag
+def get_block_with_arg(block_name, arg):
+    block_library = BlockLibrary()
+    block_library.autodiscover()
+    try:
+        return block_library.get_block(block_name)(arg)
+    except NotInLibraryException:
+        return ''
